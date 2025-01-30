a27a6728247502eb3ba6c134bd2743402b484881
Pieter Wigboldus <pieter.wigboldus@zilliz.nl>
2025-01-30 21:48:25 +0100

    Also export all rules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-export {default} from './eslint.config.js'
+import trojsRules from './rules/tro.js'
+import jsdocRules from './rules/jsdoc.js'
+import nodeRules from './rules/node.js'
+import sonarRules from './rules/sonar.js'
+import stylisticRules from './rules/stylistic.js'
+import importRules from './rules/import.js'
+import promiseRules from './rules/promise.js'
+import eslintConfig from './eslint.config.js'
+const rules = { trojs: trojsRules, jsdoc: jsdocRules, node: nodeRules, sonar: sonarRules, format: stylisticRules, import: importRules, promise: promiseRules}
+export { rules, eslintConfig as default }
