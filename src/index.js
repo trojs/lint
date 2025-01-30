17f356c5686bb1174b747abbd6dd7089ed08fa4f
Pieter Wigboldus <pieter.wigboldus@zilliz.nl>
2025-01-30 21:50:37 +0100

    Export also the plugins

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import jsdocPlugin from 'eslint-plugin-jsdoc'
+import importPlugin from 'eslint-plugin-import-x'
+import nodePlugin from 'eslint-plugin-n'
+import promisePlugin from 'eslint-plugin-promise'
+import sonarjsPlugin from 'eslint-plugin-sonarjs'
+import stylisticPlugin from '@stylistic/eslint-plugin'
+import stylisticJsPlugin from '@stylistic/eslint-plugin-js'
 import trojsRules from './rules/tro.js'
 import jsdocRules from './rules/jsdoc.js'
 import nodeRules from './rules/node.js'
@@ -6,5 +13,22 @@ import stylisticRules from './rules/stylistic.js'
 import importRules from './rules/import.js'
 import promiseRules from './rules/promise.js'
 import eslintConfig from './eslint.config.js'
-const rules = { trojs: trojsRules, jsdoc: jsdocRules, node: nodeRules, sonar: sonarRules, format: stylisticRules, import: importRules, promise: promiseRules}
-export { rules, eslintConfig as default }
+const rules = {
+  trojs: trojsRules,
+  jsdoc: jsdocRules,
+  node: nodeRules,
+  sonar: sonarRules,
+  format: stylisticRules,
+  import: importRules,
+  promise: promiseRules
+}
+const plugins = {
+  stylistic: stylisticPlugin,
+  '@stylistic/js': stylisticJsPlugin,
+  'import-x': importPlugin,
+  jsdoc: jsdocPlugin,
+  n: nodePlugin,
+  promise: promisePlugin,
+  sonarjs: sonarjsPlugin
+}
+export { rules, plugins, eslintConfig as default }
