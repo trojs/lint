5b6d7bcf4aee780d702ce1a99cdfea4984af5a75
Pieter Wigboldus <pieter.wigboldus@zilliz.nl>
2025-02-04 14:21:00 +0100

    Add Default Eslint JS rules

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import promisePlugin from 'eslint-plugin-promise'
 import sonarjsPlugin from 'eslint-plugin-sonarjs'
 import stylisticPlugin from '@stylistic/eslint-plugin'
 import stylisticJsPlugin from '@stylistic/eslint-plugin-js'
+import pluginJs from '@eslint/js'
 import trojsRules from './rules/tro.js'
 import jsdocRules from './rules/jsdoc.js'
 import nodeRules from './rules/node.js'
@@ -13,6 +14,8 @@ import stylisticRules from './rules/stylistic.js'
 import importRules from './rules/import.js'
 import promiseRules from './rules/promise.js'
 import eslintConfig from './eslint.config.js'
+
+const jsRules = pluginJs.configs.recommended.rules
 const rules = {
   trojs: trojsRules,
   jsdoc: jsdocRules,
@@ -21,7 +24,9 @@ const rules = {
   format: stylisticRules,
   import: importRules,
   promise: promiseRules,
+  js: jsRules,
   all: {
+    ...jsRules,
     ...importRules,
     ...trojsRules,
     ...jsdocRules,
