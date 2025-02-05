0e253bcc293fdd2475e224c3b39ef8f68c7418ad
Pieter Wigboldus <pieter.wigboldus@zilliz.nl>
2025-02-05 22:41:06 +0100

    Finetuning formatter

--- src/rules/stylistic.js
+++ src/rules/stylistic.js
@@ -20,7 +20,7 @@ export default {
   '@stylistic/space-before-function-paren': [
     'error',
     {
-      anonymous: 'never',
+      anonymous: 'always',
       asyncArrow: 'always',
       named: 'always'
     }
